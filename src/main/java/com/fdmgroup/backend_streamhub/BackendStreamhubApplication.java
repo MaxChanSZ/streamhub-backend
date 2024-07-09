d88e0122bf1a0785d63eaf9f2745be9aec7f7b06
handy.thong <handythong@outlook.com>
2024-07-09 16:59:35 +0800

    initial project commit

---
+++ src/main/java/com/fdmgroup/backend_streamhub/BackendStreamhubApplication.java
@@ -0,0 +1,14 @@
+package com.fdmgroup.backend_streamhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStreamhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendStreamhubApplication.class, args);
+		System.out.println("testing running hello world");
+	}
+
+}
---
+++ src/test/java/com/fdmgroup/backend_streamhub/BackendStreamhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.backend_streamhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStreamhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
