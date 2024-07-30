a9811f14e8784fbe6ab3197fc1b359631103d08d
handy.thong <handythong@outlook.com>
2024-07-30 15:57:47 +0800

    initial setup BE

---
+++ src/main/java/com/fdmgroup/backend_streamhub/authenticate/service/AccountService.java
@@ -0,0 +1,7 @@
+package com.fdmgroup.backend_streamhub.authenticate.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+}
---
+++ src/main/java/com/fdmgroup/backend_streamhub/videostream/controller/VideoController.java
@@ -0,0 +1,7 @@
+package com.fdmgroup.backend_streamhub.videostream.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VideoController {
+}
