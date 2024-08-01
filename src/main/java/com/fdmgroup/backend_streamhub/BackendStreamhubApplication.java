2937857e3cf34c472f1548229c05e7fbcf3a6fa0
mayjiayi <mayjiayiw@gmail.com>
2024-08-01 16:43:06 +0800

    Changed printing message to signify backend running

--- src/main/java/com/fdmgroup/backend_streamhub/BackendStreamhubApplication.java
+++ src/main/java/com/fdmgroup/backend_streamhub/BackendStreamhubApplication.java
@@ -10,7 +10,7 @@ public class BackendStreamhubApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendStreamhubApplication.class, args);
 
-		System.out.println("testing running hello world");
+		System.out.println("Streamhub backend has started running.");
 		VideoProcessingService service = new VideoProcessingService();
 		service.mp4ToHls("steamboatwillie_001.webm");
 	}
