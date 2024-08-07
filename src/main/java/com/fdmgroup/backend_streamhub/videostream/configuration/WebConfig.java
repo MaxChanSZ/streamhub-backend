d7a5ed1fe9e86b12ee53c78952904abf77f53925
mayjiayi <mayjiayiw@gmail.com>
2024-08-07 09:33:26 +0800

    added extra configurations to deal with CORS policy

--- src/main/java/com/fdmgroup/backend_streamhub/videostream/configuration/WebConfig.java
+++ src/main/java/com/fdmgroup/backend_streamhub/videostream/configuration/WebConfig.java
@@ -20,6 +20,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowCredentials(true);
     }
 }
