10b0b3dacb22443929381d1c2287569a2832e1e0
S.Umar <shumarb@outlook.com>
2024-08-16 09:52:29 +0800

    Removed password availability check

--- src/main/java/com/fdmgroup/backend_streamhub/authenticate/exceptions/UnavailablePasswordException.java
+++
@@ -1,8 +0,0 @@
-/**
- * Exception thrown for unavailable password encountered during registration.
- */
-
-package com.fdmgroup.backend_streamhub.authenticate.exceptions;
-
-public class UnavailablePasswordException extends Exception {
-}
