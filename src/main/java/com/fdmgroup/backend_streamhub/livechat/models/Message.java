b815554fc174b572bd3afcff1afaf6a541d2855f
Timothy Chai <timothy.chai@fdmgroup.com>
2024-08-20 13:51:24 +0800

    Create Message class

---
+++ src/main/java/com/fdmgroup/backend_streamhub/livechat/models/Message.java
@@ -0,0 +1,9 @@
+package com.fdmgroup.backend_streamhub.livechat.models;
+
+public class Message {
+    private MessageType type;
+    private String content;
+    private String sender;
+    private String sessionId;
+}
+
