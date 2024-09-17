1c91bd44932f24bb5af0b56d48f9d8990cc312d8
Timothy Chai <159506546+tchai17@users.noreply.github.com>
2024-09-17 14:10:16 +0800

    Fix May's :shit:

--- src/main/java/com/fdmgroup/backend_streamhub/watchpartysession/controller/WatchPartyController.java
+++ src/main/java/com/fdmgroup/backend_streamhub/watchpartysession/controller/WatchPartyController.java
@@ -3,7 +3,6 @@ package com.fdmgroup.backend_streamhub.watchpartysession.controller;
 import com.fdmgroup.backend_streamhub.watchpartysession.dto.CreateWatchPartyRequest;
 import com.fdmgroup.backend_streamhub.watchpartysession.model.WatchParty;
 import com.fdmgroup.backend_streamhub.watchpartysession.service.WatchPartyService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,17 +12,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/watch-party")
 public class WatchPartyController {
 
-    @Autowired
-    WatchPartyService watchPartyService;
+  @Autowired WatchPartyService watchPartyService;
 
-    @GetMapping("/create")
-    public ResponseEntity<WatchParty> createWatchParty(@RequestBody CreateWatchPartyRequest createWatchPartyRequest) {
-        System.out.println(createWatchPartyRequest.getPartyName());
-        System.out.println(createWatchPartyRequest.getAccountID());
-        WatchParty watchParty = watchPartyService.createWatchParty(createWatchPartyRequest.getPartyName(), createWatchPartyRequest.getAccountID());
-        //return ResponseEntity.ok(watchParty);
-        return ResponseEntity.status(HttpStatus.CREATED).body(watchParty);
-    }
-
-
+  @PostMapping("/create")
+  public ResponseEntity<WatchParty> createWatchParty(
+      @RequestBody CreateWatchPartyRequest createWatchPartyRequest) {
+    System.out.println(createWatchPartyRequest.getPartyName());
+    System.out.println(createWatchPartyRequest.getAccountID());
+    WatchParty watchParty =
+        watchPartyService.createWatchParty(
+            createWatchPartyRequest.getPartyName(), createWatchPartyRequest.getAccountID());
+    // return ResponseEntity.ok(watchParty);
+    return ResponseEntity.status(HttpStatus.CREATED).body(watchParty);
+  }
 }
