58c7c3aad0633eac59a74adb81233c7f6fa857b8
LeJared95 <122983113+LeJared95@users.noreply.github.com>
2024-09-23 10:06:40 +0800

    Update to match main

--- src/main/java/com/fdmgroup/backend_streamhub/quartz_scheduler/jobs/InitJobs.java
+++ src/main/java/com/fdmgroup/backend_streamhub/quartz_scheduler/jobs/InitJobs.java
@@ -2,40 +2,40 @@ package com.fdmgroup.backend_streamhub.quartz_scheduler.jobs;
 
 import com.fdmgroup.backend_streamhub.authenticate.exceptions.*;
 import com.fdmgroup.backend_streamhub.quartz_scheduler.service.JobService;
+import java.util.logging.Logger;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.lang.NonNull;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Component
 public class InitJobs extends QuartzJobBean {
 
-    private static final Logger logger = LoggerFactory.getLogger(InitJobs.class);
-
-    private final JobService jobService;
-
-    @Autowired
-    public InitJobs(JobService jobService) {
-        this.jobService = jobService;
-    }
+    @Autowired private JobService jobService;
+    private final Logger logger = Logger.getLogger(InitJobs.class.getName());
 
     @Override
-    protected void executeInternal(@NonNull JobExecutionContext context) throws JobExecutionException {
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
         try {
-            logger.info("Executing InitJobs");
+            logger.info("InitJobsExecutedSuccessfully");
             this.jobService.executeInitJobs();
-            logger.info("InitJobs executed successfully");
-        } catch (InvalidEmailAddressException | UnavailableEmailAddressException |
-                 InvalidPasswordException | InvalidUsernameException |
-                 UnavailableUsernameException | UnavailablePasswordException e) {
-            logger.error("Error executing InitJobs: {}", e.getMessage());
-            throw new JobExecutionException("Failed to execute InitJobs", (Throwable) e, false);
+        } catch (InvalidEmailAddressException e) {
+            throw new RuntimeException(e);
+        } catch (UnavailableEmailAddressException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidPasswordException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidUsernameException e) {
+            throw new RuntimeException(e);
+        } catch (UnavailableUsernameException e) {
+            throw new RuntimeException(e);
         }
     }
+    // commented out missing exception
+    //        catch (UnavailablePasswordException e) {
+    //            throw new RuntimeException(e);
+    //        }
 }
 
 
