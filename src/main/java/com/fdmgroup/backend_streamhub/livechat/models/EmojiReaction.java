e50a93a0e7088d6ebd8b0d6eb91aab6d04e82021
May Lwin <may.lwin@fdmgroup.com>
2024-09-30 16:32:18 +0800

    removed unnecessary emoji attributes

--- src/main/java/com/fdmgroup/backend_streamhub/livechat/models/EmojiReaction.java
+++ src/main/java/com/fdmgroup/backend_streamhub/livechat/models/EmojiReaction.java
@@ -3,4 +3,4 @@ package com.fdmgroup.backend_streamhub.livechat.models;
 import java.time.LocalDateTime;
 
 public record EmojiReaction(
-    String ID, String TYPE, String SESSION_ID, String SENDER, LocalDateTime TIMESTAMP) {}
+    String TYPE, String SESSION_ID, String SENDER, LocalDateTime TIMESTAMP) {}
