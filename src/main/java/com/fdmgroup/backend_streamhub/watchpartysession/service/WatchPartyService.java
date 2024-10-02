2bb18db94e816b77c7980b75ae7a2df4635bfaf0
S.Umar <shumarb@outlook.com>
2024-10-02 10:44:40 +0800

    Added backend logic on adding username to list of participants of a Watch Party, removed unused imports

--- src/main/java/com/fdmgroup/backend_streamhub/watchpartysession/service/WatchPartyService.java
+++ src/main/java/com/fdmgroup/backend_streamhub/watchpartysession/service/WatchPartyService.java
@@ -2,11 +2,10 @@ package com.fdmgroup.backend_streamhub.watchpartysession.service;
 
 import com.fdmgroup.backend_streamhub.authenticate.model.Account;
 import com.fdmgroup.backend_streamhub.authenticate.repository.AccountRepository;
-import com.fdmgroup.backend_streamhub.videostream.repository.IVideoRepository;
-import com.fdmgroup.backend_streamhub.watchpartysession.repository.IPollOptionRepository;
-import com.fdmgroup.backend_streamhub.watchpartysession.repository.IPollRepository;
 import com.fdmgroup.backend_streamhub.watchpartysession.model.WatchParty;
 import com.fdmgroup.backend_streamhub.watchpartysession.repository.IWatchPartyRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,22 +19,37 @@ public class WatchPartyService {
     @Autowired
     AccountRepository accountRepository;
 
+    private static final Logger logger = LogManager.getLogger(WatchPartyService.class);
+
+    public void addParticipantToWatchParty(String username, String code) {
+        try {
+            logger.info("Adding participant to WatchParty (Code: {}): {}", code, username);
+            Optional<WatchParty> watchPartyOptional = findByCode(code);
+            if (watchPartyOptional.isPresent()) {
+                WatchParty watchParty = watchPartyOptional.get();
+                watchParty.addParticipant(username);
+            }
+        } catch (Exception e) {
+            logger.error("Unable to add participant to list of participants in WatchParty");
+        }
+    }
+
     public WatchParty createWatchParty(String partyName, Long accountID, String scheduledDate, String scheduledTime) {
         Optional<Account> account = accountRepository.findById(accountID);
         if (account.isPresent()) {
             WatchParty watchParty = new WatchParty();
-
             watchParty.setPartyName(partyName);
             watchParty.setAccount(account.get());
             watchParty.setScheduledDate(scheduledDate);
             watchParty.setScheduledTime(scheduledTime);
-
             return watchPartyRepository.save(watchParty);
         } else {
             throw new RuntimeException("Account not found");
         }
     }
+
     public Optional<WatchParty> findByCode(String code) {
         return watchPartyRepository.findByCode(code);
     }
+
 }
