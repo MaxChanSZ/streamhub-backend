e719981547bee78f73be478a72b959ac681ff768
Timothy Chai <159506546+tchai17@users.noreply.github.com>
2024-10-02 16:54:03 +0800

    Add ID attribute to ensure deserialization works

--- src/main/java/com/fdmgroup/backend_streamhub/livechat/models/EmojiReaction.java
+++ src/main/java/com/fdmgroup/backend_streamhub/livechat/models/EmojiReaction.java
@@ -3,4 +3,4 @@ package com.fdmgroup.backend_streamhub.livechat.models;
 import java.time.LocalDateTime;
 
 public record EmojiReaction(
-    String TYPE, String SESSION_ID, String SENDER, LocalDateTime TIMESTAMP) {}
+    String ID, String TYPE, String SESSION_ID, String SENDER, LocalDateTime TIMESTAMP) {}
