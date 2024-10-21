2869e668c24b028ae3b7c597c6c9c43a7b01e61c
farin.rahman <farin.rahman@fdmgroup.com>
2024-10-21 16:59:04 +0800

    category-and-stripe-feature-linking: done

--- src/main/java/com/fdmgroup/backend_streamhub/videostream/repository/ISeriesRepository.java
+++ src/main/java/com/fdmgroup/backend_streamhub/videostream/repository/ISeriesRepository.java
@@ -1,18 +1,27 @@
 package com.fdmgroup.backend_streamhub.videostream.repository;
 
 import com.fdmgroup.backend_streamhub.videostream.model.Series;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.FetchType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface ISeriesRepository extends JpaRepository<Series, Long> {
-    List<Series> findBySeriesTitleContaining(String title);
+    @Query(value = "SELECT * FROM series s WHERE s.series_title LIKE CONCAT('%', ?1, '%') OR s.description LIKE CONCAT('%', ?2, '%')", nativeQuery = true)
+    List<Series> findBySeriesTitleOrDescription(String title, String description);
 
     @Query(value = "SELECT s FROM Series s ORDER BY s.releaseDate DESC")
     List<Series> findTopByOrderByReleaseDateDesc(int limit);
 
     @Query("SELECT s FROM Series s ORDER BY s.rating DESC")
     List<Series> findTopRatedSeries(Pageable pageable);
+
+    List<Series> findByCategory(String category);
+
+    @ElementCollection(fetch= FetchType.EAGER)
+    @Query("SELECT DISTINCT(s.category) FROM Series s")
+    List<String> findUniqueCategories();
 
 }
--- src/main/resources/mockscript.sql
+++ src/main/resources/mockscript.sql
@@ -3,16 +3,16 @@ DESCRIBE series;
 DESCRIBE video;
 
 SELECT * FROM series;
-INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl) VALUES (1000, 'Walt Disney', 'free copyright movie', '45', '19280101', 'Steamboat Willie', 'steamboatwillie_001.jpg');
-INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl) VALUES (1001, 'Private Snafu', 'free copyright movie snafu', '46', '19430101', 'Private Snafu', 'privatesnafu_001.jpg');
-INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl) VALUES (1002, 'Douglas Fairbanks, Noah Berry', 'action movie', '42', '19250101', 'The Mark of Zorro', 'themarkofzorro_001.jpg');
-INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl) VALUES (1003, 'Charles Laughton', 'pirate movie', '41', '19450101', 'Captain Kidd', 'captainkidd_001.jpg');
-INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl) VALUES (1004, 'Walt Disney', 'cartoon movie', '49', '19230101', 'Alice Wonderland', 'alicewonderland_001.jpg');
-INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl) VALUES (1005, 'Pat Sullivan', 'cartoon cat movie', '43', '19190101', 'Feline Follies', 'felinefollies_001.jpg');
-INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl) VALUES (1006, 'Max Fleischer', 'cartoon movie', '44', '19360101', 'Popeye The Sailor Meets Sindbad The Sailor', 'popeyethesailor_001.jpg');
-INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl) VALUES (1007, 'Frank Tashlin', 'cartoon movie', '43', '19430101', 'Puss and Booty', 'pussandbooty_001.jpg');
-INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl) VALUES (1008, 'Sidney Toler', 'thriller movie', '46', '19460109', 'The Trap', 'thetrap_001.jpg');
-INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl) VALUES (1009, 'Frank Sinatra', 'detective movie', '42', '19540101', 'Suddenly', 'suddenly_001.jpg');
+INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl, category) VALUES (1000, 'Walt Disney', 'free copyright movie', '45', '19280101', 'Steamboat Willie', 'steamboatwillie_001.jpg', 'Comedy');
+INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl, category) VALUES (1001, 'Private Snafu', 'free copyright movie snafu', '46', '19430101', 'Private Snafu', 'privatesnafu_001.jpg', 'Documentary');
+INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl, category) VALUES (1002, 'Douglas Fairbanks, Noah Berry', 'action movie', '42', '19250101', 'The Mark of Zorro', 'themarkofzorro_001.jpg', 'Adventure');
+INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl, category) VALUES (1003, 'Charles Laughton', 'pirate movie', '41', '19450101', 'Captain Kidd', 'captainkidd_001.jpg', 'Adventure');
+INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl, category) VALUES (1004, 'Walt Disney', 'cartoon movie', '49', '19230101', 'Alice Wonderland', 'alicewonderland_001.jpg', 'Documentary');
+INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl, category) VALUES (1005, 'Pat Sullivan', 'cartoon cat movie', '43', '19190101', 'Feline Follies', 'felinefollies_001.jpg', 'Drama');
+INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl, category) VALUES (1006, 'Max Fleischer', 'cartoon movie', '44', '19360101', 'Popeye The Sailor Meets Sindbad The Sailor', 'popeyethesailor_001.jpg', 'Comedy');
+INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl, category) VALUES (1007, 'Frank Tashlin', 'cartoon movie', '43', '19430101', 'Puss and Booty', 'pussandbooty_001.jpg', 'Comedy');
+INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl, category) VALUES (1008, 'Sidney Toler', 'thriller movie', '46', '19460109', 'The Trap', 'thetrap_001.jpg', 'Drama');
+INSERT INTO series (id, cast, description, rating, release_date, series_title, thumbnailurl, category) VALUES (1009, 'Frank Sinatra', 'detective movie', '42', '19540101', 'Suddenly', 'suddenly_001.jpg', 'Thriller');
 
 SELECT * FROM video;
 INSERT INTO video (id, description, duration_second, episode, release_date, thumbnailurl, video_title, videourl, series_id) VALUES (1, 'first episode of steamboat willie', 460, 1, '20200101', 'steamboatwillie_001.jpg', 'steamboat willie first episode', 'steamboatwillie_001.m3u8', 1000);
